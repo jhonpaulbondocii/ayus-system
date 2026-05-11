28c31ee6bffef0d384c1b02a0461f487f799de52
JhonPaul <padikmenchik@gmail.com>
2026-05-11 17:36:30 +0800

    fix: use correct component and remove repositoryId prop in repositories page

--- src/app/admin/courses/[id]/repositories/[repositoryId]/page.tsx
+++ src/app/admin/courses/[id]/repositories/[repositoryId]/page.tsx
@@ -1,14 +1,14 @@
 // src/app/admin/courses/[id]/repositories/[repositoryId]/page.tsx
 import CourseLayout from "@/components/admin/CourseLayout";
-import AdminRepositoryPage from "@/components/admin/AdminRepositoryPage";
+import AdminCourseRepositoriesPage from "@/components/admin/AdminCourseRepositoriesPage";
 
 type Props = { params: Promise<{ id: string; repositoryId: string }> };
 
 export default async function Page({ params }: Props) {
-  const { id, repositoryId } = await params;
+  const { id } = await params;
   return (
     <CourseLayout courseId={id} activeItem="Repositories">
-      <AdminRepositoryPage courseId={id} repositoryId={repositoryId} />
+      <AdminCourseRepositoriesPage courseId={id} />
     </CourseLayout>
   );
 }
